f29c06f1d62ae3ad7459d1733d9c9dcd4ea10c04
Miao-cc <1033570101@qq.com>
2018-07-27 13:40:19 +0800

    add beam weighting module

--- beamWeighting/Beam_weighting.py
+++ beamWeighting/Beam_weighting.py
@@ -74,7 +74,6 @@ def plotWeight(Weight):
     X, Y = np.meshgrid(X, Y) 
     R = np.sqrt(X**2 + Y**2) 
     Z = np.sin(R) 
-    # 具体函数方法可用 help(function) 查看,如:help(ax.plot_surface) 
     ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='rainbow') 
 
 
